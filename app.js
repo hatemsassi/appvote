300a9f50bf0c0851589b5596a1932ec298685969
Sassi Hatem <sassi.hatem@gmail.com>
2017-10-02 21:27:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,29 +38,7 @@ var login = require('./routes/loginA0');
 var user = require('./routes/user');
 
 // // ------------------ADDED auth0 sample
-// // This will configure Passport to use Auth0
-// var strategy = new Auth0Strategy({
-//     domain:       process.env.AUTH0_DOMAIN,
-//     clientID:     process.env.AUTH0_CLIENT_ID,
-//     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-//     callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://auth0-test-jb-bizzel.c9users.io/callback'
-//   }, function(accessToken, refreshToken, extraParams, profile, done) {
-//     // accessToken is the token to call Auth0 API (not needed in the most cases)
-//     // extraParams.id_token has the JSON Web Token
-//     // profile has all the information from the user
-//     return done(null, profile);
-//   });
 
-// passport.use(strategy);
-
-// // you can use this section to keep a smaller payload
-// passport.serializeUser(function(user, done) {
-//   done(null, user);
-// });
-
-// passport.deserializeUser(function(user, done) {
-//   done(null, user);
-// });
 // // ------------------- END of ADDED auth0 sample
 
 var app = express();
